91416d350ddb13c242f58b3f599fd05fb2118888
SAM123THOMAS <samabraham555@gmail.com>
2024-07-26 15:03:50 +0530

    commit final

---
+++ Day 3/MatrixAddition 2d.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3
+{
+    internal class Class4
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[2,2], matrix2 = new int[2,2], result = new int[2,2];
+            Console.WriteLine("Enter first matrix elements:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix1[i,j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Enter second matrix elements:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix2[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            Console.WriteLine("First Matrix:");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(matrix1[i, j] + "  ");
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Second Matrix:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(matrix2[i, j] + "  ");
+
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("Result Matrix:");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(result[i, j] + "  ");
+
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
---
+++ Day 3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day_3
+{
+    internal class Program
+
+    {
+
+
+        public static void swap(ref int x, ref int y)
+        {
+            int temp;
+            temp = x;
+            x= y;
+            y = temp;
+            Console.WriteLine("Value of Numbers after swap in function x=" + x + " y=" + y);
+
+        }
+        static void Main1(string[] args)
+        {
+             int x=1; int y=2;
+            Console.WriteLine("Value of Numbers before swap x=" + x +" y="+ y);
+
+            swap(ref x,ref y);
+            Console.WriteLine("Value of Numbers after swap x=" + x + " y=" + y);
+
+        }
+    }
+}
