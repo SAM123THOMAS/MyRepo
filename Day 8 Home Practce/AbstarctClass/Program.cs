f83a247f7df87eebd5e46eabc98fa28f0d08f1a1
SAM123THOMAS <samabraham555@gmail.com>
2024-08-02 18:02:42 +0530

    practice

---
+++ Day 8 Home Practce/AbstarctClass/Program.cs
@@ -0,0 +1,45 @@
+﻿using static AbstarctClass.Program;
+
+namespace AbstarctClass
+{
+    internal class Program
+    {
+        public abstract class Animal
+        {
+            public string Name { get; set; }
+            public void  GetName(string value)
+            {
+                
+                SetName(value);                ;
+            }
+            public void SetName(string name)
+            {
+
+                Name = name;
+            }
+            
+            public abstract void Eat();
+            }
+        public class Dog: Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("Eating");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Animal[] animal = new Animal[] { new Dog() };
+            foreach(var anim in animal)
+            {
+                Console.WriteLine("Enter dog Name: ");
+                string value = Console.ReadLine();
+                anim.GetName(value);
+                Console.WriteLine($"{anim.Name}");
+                anim.Eat();
+                
+            }
+        }
+    }
+}
